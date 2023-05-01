221a79f80c3a3b2f1077c1deb275c87c903f321e
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-01 10:51:08 +0330

    add LoginForm class which inherits form ModelForm

--- account/forms.py
+++ account/forms.py
@@ -1,1 +1,10 @@
 from django.forms import forms
+from account.models import User
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password',]
+
+
