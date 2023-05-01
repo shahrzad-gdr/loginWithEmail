5ec8c01674eabcf98d98790b7a8deaa0bd823d6d
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-01 11:53:18 +0330

    add logout path

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from account import views as account_view
 urlpatterns = [
     path('', account_view.index, name='index'),
     path('login/', account_view.user_login, name='login'),
+    path('logout/', account_view.user_logout, name='logout'),
 
 ]
