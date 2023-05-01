27719924ee62f0c11b778ec27df73a7c65c5ad3a
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-01 11:50:02 +0330

    edit LoginForm class from ModelForm to Form

--- account/forms.py
+++ account/forms.py
@@ -1,10 +1,8 @@
 from django import forms
-from account.models import User
 
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['email', 'password',]
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.PasswordInput()
 
 
