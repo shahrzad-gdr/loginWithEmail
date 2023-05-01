8f326809a3a3f8644bb2412fd32d59ebfb5bedfd
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-01 13:25:36 +0330

    add UserUpdateForm to forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from account.models import User
+
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.PasswordInput()
 
 
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name',]
