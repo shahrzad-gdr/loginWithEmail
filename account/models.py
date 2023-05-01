320f06a12dfe4fcddb964f8f20e19f354e76432e
sh gdr <goudarzi.sharzad825@gmail.com>
2023-05-01 10:42:22 +0330

    add User model inherits form AbstractUser to models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    username = None
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(verbose_name='email address', unique=True)
+
+
+    REQUIRED_FIELDS = []
